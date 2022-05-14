1bac67e6212b6f30561b4f2ebf7bf92756fa462b
Tanvir Sakil <tanbirshakil18@gmail.com>
2022-05-15 02:23:28 +0600

    third commit4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 
 const { MongoClient, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://Travel-management:tanbir1234@cluster0.w9eor.mongodb.net/repairService?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w9eor.mongodb.net/repairService?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
  
 // parse application/x-www-form-urlencoded
